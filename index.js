79dc38debf7215d54a183ed3dfca9dfd08ec7789
YF Sun <aaronsun1989v3@gmail.com>
2024-03-24 23:09:55 +1100

    Update port and add start script for Vercel deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the environment variable if available
 
 let articleList = [];   // a list for articles posted
 let nextPostId = 1;
